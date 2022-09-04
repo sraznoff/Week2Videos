af60b15f51908b9f36102c5c0ed48a03f257f2c7
Sraznoff <sraznoff@headwatersgeographic.com>
2022-09-04 11:02:18 -0600

    Initial commit with first video finished

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(2==2);
+		System.out.println(2>5);
+		System.out.println(3 < 5);
+		System.out.println(5<=5);
+		// && and || or
+		System.out.println(2 ==2 && 3 < 5);
+		System.out.println(true && true);
+		System.out.println(true && false);
+		System.out.println(true || false);
+		System.out.println(false || false);
+	}
+
+}
